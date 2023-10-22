fcf4b3c2782fef0a9cb839859206841c00169232
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2023-10-22 13:08:45 +0000

    docker network and volume

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hello Docker")
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const port = 3000
+
+mongoose.connect("mongodb://localhost:27017/mydatadb",{})
+
+const EntrySchema = new mongoose.Schema({
+    text:String,
+    date: {type:Date,default:Date.now}
+})
+
+const Entry = mongoose.model("Entry",EntrySchema)
+
+app.get("/",async (req,res) => {
+    try {
+        const entry = Entry({text:"Hellow Friends"})
+        await entry.save()
+        res.status(200).send("Entry Saved")
+    } catch (error) {
+        res.status(500).send("Error Occureed!")
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`App running on port ${port}`);
+})
